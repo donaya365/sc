e68b44efd76058b76de48a7354b8065c2576937c
call me kill <82857286+donaya365@users.noreply.github.com>
2024-05-02 17:29:07 +0700

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -4,16 +4,16 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IP=$(wget -qO- ipinfo.io/ip);
-Green_font_preipinfo.io/ipx="\033[32m" && Red_font_preipinfo.io/ipx="\033[31m" && Green_background_preipinfo.io/ipx="\033[42;37m" && Red_background_preipinfo.io/ipx="\033[41;37m" && Font_color_sufipinfo.io/ipx="\033[0m"
-Info="${Green_font_preipinfo.io/ipx}[ON]${Font_color_sufipinfo.io/ipx}"
-Error="${Red_font_preipinfo.io/ipx}[OFF]${Font_color_sufipinfo.io/ipx}"
+clear
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[ON]${Font_color_suffix}"
+Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
 cek=$(grep -c -E "^# Autokill" /etc/cron.d/tendang)
 if [[ "$cek" = "1" ]]; then
 sts="${Info}"
 else
 sts="${Error}"
-ipinfo.io/ip
+fi
 clear
 echo -e ""
 echo -e "======================================" | lolcat
